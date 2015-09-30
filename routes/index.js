0bf95c0e6808263d8fe3b84a18a1299e89ace9d4
jichangfeng67 <jichangfeng67@gmail.com>
2015-09-30 20:47:22 +0200

    the first page to change, nothing much

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'EFREI-CHINOISE' });
 });
 
 router.get('/clashOfClans', function(req, res, next){
