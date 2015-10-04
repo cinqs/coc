bfa66102e4341c4ebc84d5b3dc673ef3825c8a0b
jichangfeng67 <jichangfeng67@gmail.com>
2015-10-04 16:34:39 +0200

    have already find a solution to query, but not find a solution to display in jade

--- routes/newspost.js
+++ routes/newspost.js
@@ -3,17 +3,30 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
-
+var url = 'mongodb://localhost:27017/posts';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var jsonOut = '';
+	MongoClient.connect(url, function(err, db){
+		var posts = db.collection('postcon').find();
+		posts.each(function(err, doc) {
+		      assert.equal(err, null);
+		      if (doc != null) {
+		      	jsonOut += doc;
+		      } ;
+		});
+	});
+
+	console.log(jsonOut);
+
+	res.render('newspost', {newspost:'news'});
 });
 
 router.post('/', function(req, res, next){
 	var newspost = req.body.newspost;
 	console.log(newspost);
 	//res.render('newspost', {newspost:newspost});
-	var url = 'mongodb://localhost:27017/posts';
+	
 	MongoClient.connect(url, function(err, db) {
 	  assert.equal(null, err);
 	  console.log("Connected correctly to server.");
