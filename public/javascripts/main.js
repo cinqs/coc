52e6ccbbf254b673b95aad1e92fc18a10c483397
jichangfeng67 <jichangfeng67@gmail.com>
2015-10-04 15:36:28 +0200

    correct the $http problem and will try to implement the mongodb module

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,8 @@
 var app = angular.module('homepage', []);
-app.controller('hpctrl', function($scope) {
+app.controller('hpctrl', function($scope, $http) {
     $scope.firstName= "John";
     $scope.lastName= "Doe";
     $scope.sendServer = function(newnews){
-    	$http.post('/newspost', {newspost:newnews})
-    }
+    	$http.post('/newspost', {newspost:newnews});
+    };
 });
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,6 @@ router.get('/clashOfClans/members', function(req, res, next){
 router.post('/newspost', function(req, res, next){
 	var newspost = req.body.newspost;
 	console.log(newspost);
-	res.render('newspost', {newspost:newspost})
+	res.render('newspost', {newspost:newspost});
 })
 module.exports = router;
