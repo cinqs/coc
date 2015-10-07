f63d70a1ec610ca8de0b21a91be9db5c8e69a1fa
jichangfeng67 <jichangfeng67@gmail.com>
2015-10-07 22:49:19 +0200

    adding the passport auth into, this commit is to carefully not lose any step

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session      = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var newspost = require('./routes/newspost');
 var member = require('./routes/member.js');
 
+var configDB = require('./config/db.js');
+
 var app = express();
+
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +33,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret:'thisisefreichinoisloginandsignup'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./routes/account.js')(app, passport);
 
 app.use('/', routes);
 app.use('/users', users);
